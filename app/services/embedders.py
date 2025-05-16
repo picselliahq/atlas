8da94ef916132632159ca4f4a54e2ba092c18d5e
PN-picsell <pierre-nicolas@picsellia.com>
2025-05-16 18:34:57 +0200

    feat: add local image and shape embedder

---
+++ app/services/embedders.py
@@ -0,0 +1,5 @@
+from services.data_extraction.image_embeddings import CLIPImageEmbedder
+from services.data_extraction.shape_embeddings import CLIPShapeEmbedder
+
+clip_image_embedder = CLIPImageEmbedder()
+clip_shape_embedder = CLIPShapeEmbedder()
